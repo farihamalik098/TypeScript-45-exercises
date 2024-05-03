00ee1dc15dcb4479f72f89d7409bc310749108b5
FARIHA MALIK <163237663+farihamalik098@users.noreply.github.com>
2024-05-03 13:39:10 -0700

    Add files via upload

---
+++ task30/main.js
@@ -0,0 +1,11 @@
+//Hello Admin: Make a array of five or more usernames, including the name 'admin'. Imagine you are writing code that will print a greeting to each user after they log in to a website. Loop through the array, and print a greeting to each user:
+//• If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report?
+var userNames = ["fahad", "ali", "zeeshan", "adeel", "Admin"];
+userNames.forEach(function (oneUser) {
+    if (oneUser === "Admin") {
+        console.log("hello ".concat(oneUser, ", would you like to see a status report?"));
+    }
+    else {
+        console.log("Hello ".concat(oneUser, ", thank you for logging in again"));
+    }
+});
